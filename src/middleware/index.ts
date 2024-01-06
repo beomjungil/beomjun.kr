e1d916966209b289fef905a437c6408e4942387e
Beomjun Gil <beomjungil@Beomjuns-MacBook-Pro.local>
2024-01-07 03:03:43 +0900

    add metadata

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -12,12 +12,3 @@ export const onRequest: MiddlewareHandler = (context, next) => {
 
   return sequence(astroI18n)(context, next);
 };
-
-// export function onRequest({ locals, request }, next) {
-//   // intercept data from a request
-//   // optionally, modify the properties in `locals`
-//   locals.title = 'New title';
-
-//   // return a Response or the result of calling `next()`
-//   return next();
-// }
